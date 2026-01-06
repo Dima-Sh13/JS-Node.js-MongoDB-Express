ea6e43565d3f265191ea8a2e8c70a8034b0516ea
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-06 11:46:15 +0100

    basic server + nodemon and npm

--- basicServer.js
+++ basicServer.js
@@ -4,12 +4,15 @@ var http = require("http");
 
 
 var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+    response.end("<h3>Hello World, im a Node.js server<h3>");
 });
 
 
 // arrancamos servidor
 
 server.listen(1337, "127.0.0.1");
+
+console.log("servidor working properly on 127.0.0.1:1337")
+
 
