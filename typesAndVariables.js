212c29d9b30cbc6a013e82861cc8413e2d776342
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-10 10:22:41 +0100

    practice

---
+++ typesAndVariables.js
@@ -0,0 +1,51 @@
+// todo esto se ejecuta en nodemon en  tiempo real
+// comando: nodemon (nombreArchivo)
+
+var numero = 1
+console.log(numero)
+console.log(typeof(numero));
+
+numero = "hello"
+console.log(typeof(numero));
+
+// string
+
+console.log("texto de prueba".split(" ")) // esto devuelve array
+
+var textoLargo = 
+    "linea1\n" +
+    "linea2\n"
+
+console.log(textoLargo);
+
+
+var objeto = {
+    numero: 1,
+    texto: "hola",
+    sumaDos: function (v) {
+        return v + 2;
+    }
+
+};
+
+console.log(objeto.numero);
+console.log(objeto.texto);
+console.log(objeto.sumaDos(2));
+
+
+//arrays
+
+var array = [
+    1,
+    "hola",
+    function(v) {return v+2},
+    {valor: "hola"}
+
+];
+
+console.log(array);
+console.log(array[0]);
+console.log(array[2](3));
+
+
+
