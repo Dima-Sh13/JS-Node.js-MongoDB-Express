e01e802dd9c3c6509e31d4e7a2734f188f265dc3
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-13 16:09:01 +0100

    practice2

--- flujoYoperadores_json.js
+++ flujoYoperadores_json.js
@@ -14,6 +14,37 @@ console.log(serializado)
 
 //des_serializar el objeto  de string a objeto
 
+serializado = serializado + "{";
 
-let desserie = JSON.parse(serializado)
-console.log(desserie)
+
+
+let leido;
+
+try{
+    leido = JSON.parse(serializado);
+} catch (err) {
+    console.log("no se pudo leer el archivo.");
+}
+
+
+if (typeof leido != "undefined") {
+    console.log("hay algo leido");
+} else {
+    console.log("no hay nada leido")
+}
+
+
+for (var i=0; i < empleado.lenguaje.length; i++) {
+    let arma = empleado.lenguaje[i];
+    console.log(empleado.nombre + (arma == "php" ? " sabe " : ": " ) + arma);
+} 
+
+
+
+array = [1,2,3,4,5,6]
+
+
+for ( let i=0; i < array.length; i++) {
+    let numero = array[i]
+    console.log((numero == 3 ? "tres" : numero))
+}
