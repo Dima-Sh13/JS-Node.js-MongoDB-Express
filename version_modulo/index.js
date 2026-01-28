7371b47b6fa2332e6b59e148d23b86feff849756
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-28 12:56:16 +0100

    practice

--- version_modulo/index.js
+++ version_modulo/index.js
@@ -1,11 +1,29 @@
 "use strict";
 
 var fs = require('fs');
-var path = require('path')
+var path = require('path');
 
 var versionModulo = function(moduleName, callBack) {
     var fichero = path.join('./node_modules', moduleName, 'package.json');
-    
+    fs.readFile(fichero, 'utf8', function(err, datos) {
+        if (err) {
+            callBack(err);
+            return;
+        }
+        //si no ha habido error
+        // convertimos contenido en objeto
+
+        var packageJson = JSON.parse(datos);
+        
+        if (!packageJson.version) {
+            callBack('No se encuentra la etiqueta de version en el archivo:' + fichero);
+            return;
+        }
+        // llamamos al callBack de finalizacion
+
+        callBack( null, packageJson.version);
+
+    });
 
 };
 
@@ -18,7 +36,7 @@ versionModulo(modulo, function(err, version) {
         console.log("hubo un error:", err);
         return;
     }
-    console.log('Es la version del modulo' + modulo + "es:" + version);
+    console.log('La version del modulo ' + modulo + " es:" + version);
 });
 
 
