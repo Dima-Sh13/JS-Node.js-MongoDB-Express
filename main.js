c5bee223256518e0aedd01d1ba53f7edfbd51a80
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-28 12:40:22 +0100

    truthy /flasy

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ let textoJSON = '{"nombre":"Thomas Anderson", "profesion":"Developer"}';
 let parseado = JSON.parse(textoJSON);
 
 console.log(parseado)
+
+
+/// Truthy and falsy
+
+// en javascript topdo es verdadero o falso
+
+// los valores que son falsy son los siguientes: falsa, 0, "", null, undefined , NaN(valor especial del tipo numbre que significa Not-a-number)
+// los arrays, funciones y los objetos vacios son siempre truthy 
+// false, 0 y "" dan true al hacer ==
+
+
+
