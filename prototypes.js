fbe28756b61763fde84a4a80bd535dfaf71778ad
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-29 11:25:56 +0100

    prototypes 2

--- prototypes.js
+++ prototypes.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // ejemplos de prototipo
 
 function Persona(name) {
@@ -11,3 +13,21 @@ Persona.prototype.saluda = function() {
 
 // todas los objetos de persona van a tener la funcion de saludar
 // incluso las que ya estan creadas
+
+
+// example de prototype
+
+
+function Persona(nombre) {
+    this.name = nombre;
+}
+
+var persona = new Persona('Neo');
+
+Persona.prototype.saluda = function() {
+    console.log('Hola mi nombre es ' + this.name);
+}
+
+
+console.log(persona.name);
+persona.saluda()
