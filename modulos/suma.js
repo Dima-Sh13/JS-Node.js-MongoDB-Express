a08a53dc29e1defa656f825814eed16c927a77b4
Dima Shalupnya <dshalupnya@gmail.com>
2026-02-02 22:40:12 +0100

    objects

--- modulos/suma.js
+++ modulos/suma.js
@@ -11,3 +11,22 @@ module.exports = {
         return a - b;
     }
 }
+
+
+var calculadora = {
+    suma: function(a,b) {
+        return a + b;
+    },
+    resta: function(a,b) {
+        return a - b;
+    },
+    mult: function(a,b) {
+        return a * b;
+   },
+   div: function(a,b) {
+        return a / b;
+   }    
+}
+
+
+console.log(calculadora.mult(5,5))
