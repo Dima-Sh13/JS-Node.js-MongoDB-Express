078749b484e0a41c5ef21966a3f91b795ff7424a
Dima Shalupnya <dshalupnya@gmail.com>
2026-02-03 13:04:16 +0100

    my module example

--- modulos/suma.js
+++ modulos/suma.js
@@ -5,28 +5,25 @@ var suma = function(a,b) {
 };
 
 
-module.exports = {
-    suma: suma,
-    resta: function(a,b) {
-        return a - b;
-    }
-}
+module.exports = calculadora
 
 
 var calculadora = {
     suma: function(a,b) {
-        return a + b;
+        return console.log(a + b);
     },
     resta: function(a,b) {
-        return a - b;
+        return console.log(a - b);
     },
     mult: function(a,b) {
-        return a * b;
+        return console.log(a * b);
    },
    div: function(a,b) {
-        return a / b;
+        return console.log(a / b);
    }    
 }
 
-
-console.log(calculadora.mult(5,5))
+calculadora.mult(5,5)
+calculadora.resta(5,5)
+calculadora.suma(5,5)
+calculadora.div(5,5)
