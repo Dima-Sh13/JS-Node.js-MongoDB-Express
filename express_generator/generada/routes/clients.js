a3c85df8bfcf675fe84a3d9aad9fd5205ce645a7
Dima Shalupnya <dshalupnya@gmail.com>
2026-02-08 20:23:49 +0100

    double send error+

--- express_generator/generada/routes/clients.js
+++ express_generator/generada/routes/clients.js
@@ -5,8 +5,18 @@ var router = express.Router();
 
 router.use(function(req, res,next) {
     console.log('middleware de router clientes');
+    /*
+    res.send('respuesta');
     next();
-
+     esto daria error pero no en este middleware si no en el siguiente que tenga un send.
+    que no podr responder poerque ya se ha respondido aqui. para evitarlo */
+    if (condicion) {
+        res.send('respuesta')
+        return; // no debemos olvidar el return para que el coddigo de la funcion no siga ejecutandose.
+                // tambien valdria un else
+                // pero en javascript es mucho mas comun este tipo de estructura
+    }
+    next(); 
 });
 
 
